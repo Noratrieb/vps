5ec5a3e64ee40d4285dd33621925821d4dc253ed
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-09-01 20:49:33 +0200

    localhost nginx

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,13 +2,24 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+if [ $STAGE = "localhost" ] ;
+then
+    echo "INFO Running on localhost"
+    NGINX_CONF="nginx.local.conf"
+    CERT_VOLUME=""
+else
+    echo "INFO Running on prod"
+    NGINX_CONF="nginx.conf"
+    CERT_VOLUME="-v=/etc/letsencrypt:/etc/nginx/certs"
+fi
+
 if docker container inspect nginx > /dev/null 2>&1 ;
 then
-    echo "nginx container exists already..."
+    echo "INFO nginx container exists already..."
 else
-    docker run -d -p 80:80 -p 443:443 --restart=always --name nginx \
-        -v "$SCRIPT_DIR/nginx.conf:/etc/nginx/nginx.conf:ro" \
-        -v "/etc/letsencrypt:/etc/nginx/certs" \
-        --net internal \
+    docker run -d -p 80:80 -p 443:443 --restart=always --name=nginx \
+        -v="$SCRIPT_DIR/$NGINX_CONF:/etc/nginx/nginx.conf:ro" \
+        $CERT_VOLUME \
+        --net=internal \
         nginx:latest
 fi
