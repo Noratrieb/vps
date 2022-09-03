99d5edbb94933f0a7d9e7fa716553da3c9fe5791
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-09-03 20:20:44 +0200

    ports

--- nginx/run.sh
+++ nginx/run.sh
@@ -6,20 +6,20 @@ if [ "$STAGE" = "localhost" ] ;
 then
     echo "INFO Running on localhost"
     NGINX_CONF="nginx.local.conf"
-    CERT_VOLUME=""
+    EXTRA_ARGS="-p 80:80"
 else
     echo "INFO Running on prod"
     NGINX_CONF="nginx.conf"
-    CERT_VOLUME="-v=/etc/letsencrypt:/etc/nginx/certs"
+    EXTRA_ARGS="-v=/etc/letsencrypt:/etc/nginx/certs -p 443:443"
 fi
 
 if docker container inspect nginx > /dev/null 2>&1 ;
 then
     echo "INFO nginx container exists already..."
 else
-    docker run -d -p 80:80 -p 443:443 --restart=always --name=nginx \
+    docker run -d --restart=always --name=nginx \
         -v="$SCRIPT_DIR/$NGINX_CONF:/etc/nginx/nginx.conf:ro" \
-        $CERT_VOLUME \
+        $EXTRA_ARGS \
         --net=internal \
         nginx:latest
 fi
