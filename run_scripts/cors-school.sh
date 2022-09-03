b26f72a59e92cfb875cc40654cc86311ef2f7014
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-09-03 15:46:36 +0200

    cors

--- run_scripts/cors-school.sh
+++ run_scripts/cors-school.sh
@@ -1,1 +1,8 @@
+# frontend
 docker run --net internal --name cors-school-frontend -d --restart=always docker.nilstrieb.dev/cors-school-frontend:1.0.0
+
+# postgres
+docker run --net internal -d --name cors-postgres -e POSTGRES_PASSWORD=hugo58hugo -e POSTGRES_DB=davinci postgres
+
+# backend
+docker run --net internal -d --name cors-school-backend -v -e DATABASE_URL=postgres://postgres:hugo58hugo@cors-postgres/davinci -e RUST_LOG=info -e JWT_SECRET=redacted
