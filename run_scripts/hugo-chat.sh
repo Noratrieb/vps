85c26af9e5d1e79de6bb2ff76e3df2bb8df73d34
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-09-10 21:42:14 +0200

    hugo

--- run_scripts/hugo-chat.sh
+++ run_scripts/hugo-chat.sh
@@ -1,1 +1,1 @@
-docker run --net internal --name hugo-chat-frontend -d --restart=always docker.nilstrieb.dev/hugo-chat-frontend:1.0.0
+docker run --net internal --name hugo-chat-frontend -d --restart=always docker.nilstrieb.dev/hugo-chat-frontend:1.0
