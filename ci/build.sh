3e8ea95dea7f86b8cd4a5dd245b5d17766d68c7a
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-02-09 21:46:29 +0100

    fix build

--- ci/build.sh
+++ ci/build.sh
@@ -13,5 +13,5 @@ CURRENT_COMMIT=$(git rev-parse HEAD | cut -c1-8)
 echo "Latest commit of $APP is $CURRENT_COMMIT"
 
 IMAGE_FULL_NAME="docker.nilstrieb.dev/$APP:$CURRENT_COMMIT"
-docker build -t "$IMAGE_FULL_NAME"
+docker build . -t "$IMAGE_FULL_NAME"
 docker push "$IMAGE_FULL_NAME"
