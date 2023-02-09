df45dcbb9f2d79fcb41e0350f7a6b70be6de5fd8
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-02-09 21:41:35 +0100

    ci

---
+++ ci/build.sh
@@ -0,0 +1,15 @@
+#/usr/bin/env bash
+
+APP="$1"
+
+echo "Checking out $APP"
+
+git clone "https://github.com/Nilstrieb/$APP.git" ../app
+cd ../app
+
+CURRENT_COMMIT=$(git rev-parse HEAD | cut -c1-8)
+echo "Latest commit of $APP is $CURRENT_COMMIT"
+
+IMAGE_FULL_NAME="docker.nilstrieb.dev/$APP:$CURRENT_COMMIT"
+docker build -t "$IMAGE_FULL_NAME"
+docker push "$IMAGE_FULL_NAME"
