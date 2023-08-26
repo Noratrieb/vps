22b8ec58bd053856bd2973bcfce2e1da1ef0b75c
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-08-26 21:55:46 +0200

    add killua-bot

--- ci/build.sh
+++ ci/build.sh
@@ -12,6 +12,8 @@ cd ../app
 CURRENT_COMMIT=$(git rev-parse HEAD | cut -c1-8)
 echo "Latest commit of $APP is $CURRENT_COMMIT"
 
-IMAGE_FULL_NAME="docker.nilstrieb.dev/$APP:$CURRENT_COMMIT"
+IMAGE_PREFIX="docker.nilstrieb.dev/$APP"
+IMAGE_FULL_NAME="$IMAGE_PREFIX:$CURRENT_COMMIT"
+
 docker build . -t "$IMAGE_FULL_NAME"
 docker push "$IMAGE_FULL_NAME"
