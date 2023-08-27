93fd3e2d543b7e7411ff8464699a3748f1cc05ba
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-08-27 14:38:05 +0200

    move

--- registry/README.md
+++
@@ -1,6 +0,0 @@
-- https://docs.docker.com/registry/deploying/
-- https://docs.docker.com/registry/configuration/
-
-```sh
-sudo htpasswd -cB /etc/.htpasswd username
-```
--- run_scripts/cargo-bisect-rustc.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu pipefail
-
-docker run -d --name cargo-bisect-rustc-service --net=internal --restart=always  \
-                                "-v=/apps/cargo-bisect-rustc-service/db:/app/db" \
-                                "-e=SQLITE_DB=/app/db/db.sqlite" "-e=RUST_LOG=debug" \
-                                 docker.nilstrieb.dev/cargo-bisect-rustc-service:1.8
