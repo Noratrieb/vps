ab7d6a91d6138dc7e7a25c15313f2088732449c0
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2023-08-27 12:58:16 +0200

    cors-school

---
+++ new/vps2/README.md
@@ -0,0 +1,7 @@
+## ports
+
+- 5000: registry
+- 5001: hugo-chat server
+- 5002: hugo-chat client
+- 5003: cors-school server
+- 5004: cors-school client
--- run_scripts/cors-school.sh
+++ run_scripts/cors-school.sh
@@ -1,13 +1,18 @@
 set -eu pipefail
 
 # frontend
-docker run --net internal --name cors-school-frontend -d --restart=always docker.nilstrieb.dev/cors-school-frontend:1.0.0
+docker run --net internal --name cors-school-frontend -d \
+    --restart=always docker.nilstrieb.dev/cors-school-frontend:1.0.0
 
 # postgres
-docker run --net internal -d --name cors-postgres -e POSTGRES_PASSWORD=hugo58hugo -e POSTGRES_DB=davinci postgres
+docker run --net internal -d --name cors-postgres \
+    -e POSTGRES_PASSWORD=hugo58hugo -e POSTGRES_DB=davinci postgres
 
 # backend
-docker run --net internal -d --name cors-school-backend -e DATABASE_URL=postgres://postgres:hugo58hugo@cors-postgres/davinci -e RUST_LOG=info -e JWT_SECRET='redacted' docker.nilstrieb.dev/cors-school-backend:1.0
+docker run --net internal -d --name cors-school-backend \
+    -e DATABASE_URL=postgres://postgres:hugo58hugo@cors-postgres/davinci \
+    -e RUST_LOG=info -e JWT_SECRET='redacted' docker.nilstrieb.dev/cors-school-backend:1.0
 
 # bot
-docker run --net internal -d --name cors-school-bot -v "$(pwd)/env:/.env" docker.nilstrieb.dev/cors-school-bot:1.0
+docker run --net internal -d --name cors-school-bot \
+    -v "$(pwd)/env:/.env" docker.nilstrieb.dev/cors-school-bot:1.0
