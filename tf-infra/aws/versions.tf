776e4fb1cff7c9169566942b5f5238794c95002a
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2024-07-13 17:21:21 +0200

    use s3 backend

--- tf-infra/aws/versions.tf
+++ tf-infra/aws/versions.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.35.0"
     }
   }
+  backend "s3" {
+    bucket = "nilstrieb-states"
+    key = "aws-terraform.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 provider "aws" {
