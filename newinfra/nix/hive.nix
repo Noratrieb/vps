90a578e4ccee83110a769bc474d35e8ca5c51ad8
Noratrieb <48135649+Noratrieb@users.noreply.github.com>
2024-08-21 22:14:33 +0200

    update blog

--- newinfra/nix/hive.nix
+++ newinfra/nix/hive.nix
@@ -11,7 +11,7 @@
       nixpkgs-unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d04953086551086b44b6f3c6b7eeb26294f207da.tar.gz") { }; # nixos-unstable 2024-08-05
 
       website = import (fetchTarball "https://github.com/Noratrieb/website/archive/ab44e5ef7586a220fc1d251bda333a8752bb7783.tar.gz");
-      blog = fetchTarball "https://github.com/Noratrieb/nilstrieb.github.io/archive/8162ce0cff29f940507032be6b0692290d73594c.tar.gz";
+      blog = fetchTarball "https://github.com/Noratrieb/nilstrieb.github.io/archive/6874f2a543d9e068a43afe78c403c23766c16cb1.tar.gz";
       slides = fetchTarball "https://github.com/Noratrieb/slides/archive/0401f35c22b124b69447655f0c537badae9e223c.tar.gz";
 
       pretense = import (fetchTarball "https://github.com/Noratrieb/pretense/archive/270b01fc1118dfd713c1c41530d1a7d98f04527d.tar.gz");
